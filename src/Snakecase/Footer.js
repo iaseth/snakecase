144e443e7c258237dbc91c518e75d93c636d0775
Ankur Seth <iankurseth@gmail.com>
2022-02-17 10:09:29 +0530

    Updated Footer styles

--- src/Snakecase/Footer.js
+++ src/Snakecase/Footer.js
@@ -5,7 +5,7 @@ import {ExternalLink} from '../Utils';
 export default function Footer () {
 
 	return (
-		<div className="Footer bg-slate-800 px-4 py-4 text-center">
+		<div className="Footer bg-slate-800 px-4 py-5 text-sm text-center">
 			<h2>Snakecase by <ExternalLink href="https://github.com/iaseth">Ankur Seth</ExternalLink>. Hosted on <ExternalLink href="https://github.com/iaseth/snakecase">GitHub</ExternalLink>.</h2>
 		</div>
 	);
